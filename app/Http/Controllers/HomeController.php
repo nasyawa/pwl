f625a158c3349c1ecde209c8497583e9ef019ac2
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2023-03-30 21:17:15 +0700

    25_Nasyawa_PWL_Jobsheet6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,31 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home(){
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
         return view('beranda');
     }
-
-    public function kuliah(){
-        return view('sekolah');
-    }
-
-    public function profile(){
+    public function profile()
+    {
         return view('profile');
+    }
+    public function kuliah()
+    {
+        return view('sekolah');
     }
 }
